5a403f7d7df5cfb921aad06ccae839e3345a3e8a
Dabble <85852991+Daniel8871@users.noreply.github.com>
2021-10-05 20:48:47 -0500

    Submitting completion of part 1

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,52 @@
+// from data.js
+var tableData = data;
+
+// select the table 
+var tbody = d3.select("tbody");
+
+// Building table function 
+function buildTable(x) {
+    x.forEach(ufoReport => {
+
+        // Adding rows 
+        var row = tbody.append("tr");
+        count += 1;        
+
+        // Gathering data and appending to cells
+        Object.entries(ufoReport).forEach(([key,value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        })
+})};
+
+// build default table 
+buildTable(tableData);
+
+// Prepping search function 
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Building filtered table 
+function runEnter() {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    // Grab search date 
+    var inputValue = inputElement.property("value");
+
+    // Deleting original table 
+    d3.selectAll("td").remove();
+
+    function dateSearch(tableDates) {
+        return tableDates.datetime == inputValue;
+    }
+
+    // Filtering the table 
+    var searchedData = tableData.filter(dateSearch);
+
+    buildTable(searchedData);
+}
